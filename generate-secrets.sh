ab9421c79c403df0029ff508fbfab6c69b9afcc4
Travis Redfield <travis.redfield@slalom.com>
2017-01-23 12:17:01 -0800

    add color

--- generate-secrets.sh
+++ generate-secrets.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# output colors
+red='\033[0;31m'
+green='\033[0;32m'
+reset='\033[0m'
+
 auth_user=$(echo concourse | base64)
 auth_password=$(openssl rand -base64 32)
 postgres_password=$(openssl rand -base64 32)
@@ -9,20 +14,24 @@ postgres_password=$(openssl rand -base64 32)
 keydir=./concourse-keys
 mkdir -p $keydir
 
+echo -e "${green}Create key files${reset}"
 ssh-keygen -t rsa -f $keydir/tsa_host_key -N ''
 ssh-keygen -t rsa -f $keydir/session_signing_key -N ''
 ssh-keygen -t rsa -f $keydir/worker_key -N ''
 
+echo -e "${green}Set secret vars${reset}"
 tsa_host_key=$(cat $keydir/tsa_host_key | base64)
 session_signing_key=$(cat $keydir/session_signing_key | base64)
 tsa_worker_private_key=$(cat $keydir/worker_key | base64)
 tsa_authorized_keys=$(cat $keydir/worker_key.pub | base64)
 tsa_public_key=$(cat $keydir/tsa_host_key.pub | base64)
 
+echo -e "${green}Remove key files directory${reset}"
 rm -rf $keydir
 
 secrets_file=$(mktemp concourse-secrets.XXXXXX)
 
+echo -e "${green}Generate $secrets_file file for kubernetes${reset}"
 cat > $secrets_file <<-EOF
 apiVersion: v1
 kind: Secret
@@ -46,5 +55,7 @@ data:
 
 EOF
 
+echo -e "${green}Call kubectl create on $secrets_file${reset}"
 kubectl create -f $secrets_file
+echo -e "${green}Remove $secrets_file${reset}"
 rm $secrets_file
